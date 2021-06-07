5c1d505add161387faba13030498d89a04a3f35f
BAILLEUL <bailleul.sacha@gmail.com>
2021-06-07 22:32:57 +0200

    [chore]: Add logs

--- CodingChainWorker.Infrastructure/Services/Processes/ProcessService.cs
+++ CodingChainWorker.Infrastructure/Services/Processes/ProcessService.cs
@@ -44,20 +44,35 @@ namespace CodingChainApi.Infrastructure.Services.Processes
             await ExecuteParticipation(participation);
         }
 
-        public Task PrepareParticipationExecution(ParticipationAggregate participation)
+        public async Task PrepareParticipationExecution(ParticipationAggregate participation)
         {
-            return _directoryService.GetTemplateDirectoryByParticipation(participation).ToTask();
+            Logger.LogInformation(
+                "Template preparation started for participation : {ParticipationId} on language : {Language} ",
+                participation.Id, participation.Language);
+            await _directoryService.GetTemplateDirectoryByParticipation(participation).ToTask();
+            Logger.LogInformation(
+                "Template preparation ended for participation : {ParticipationId} on language : {Language} ",
+                participation.Id, participation.Language);
         }
 
         public Task CleanParticipationExecution(ParticipationAggregate participation)
         {
+            Logger.LogInformation(
+                "Template deletion started for participation : {ParticipationId} on language : {Language} ",
+                participation.Id, participation.Language);
             _directoryService.DeleteParticipationDirectory(participation);
+            Logger.LogInformation(
+                "Template deletion ended for participation : {ParticipationId} on language : {Language} ",
+                participation.Id, participation.Language);
             return Task.CompletedTask;
         }
 
 
-        protected async Task ExecuteParticipation(ParticipationAggregate participation)
+        protected Task ExecuteParticipation(ParticipationAggregate participation)
         {
+            Logger.LogInformation(
+                "Tests execution started for participation : {ParticipationId} on language : {Language} ",
+                participation.Id, participation.Language);
             using var process = new Process
             {
                 StartInfo =
@@ -75,27 +90,34 @@ namespace CodingChainApi.Infrastructure.Services.Processes
             process.ErrorDataReceived += (o, e) =>
             {
                 participation.AddError(e.Data ?? "");
-                Logger.LogDebug("{Error}", e.Data);
             };
             process.OutputDataReceived += (o, e) =>
             {
                 participation.AddOutput(e.Data ?? "");
-                Logger.LogDebug("{Output}", e.Data);
             };
             process.Exited += (o, e) => { Logger.LogDebug("Process ended"); };
             process.Start();
             process.BeginOutputReadLine();
             process.BeginErrorReadLine();
-            await process.WaitForExitAsync();
             process.WaitForExit();
+            Logger.LogInformation(
+                "Tests execution ended for participation : {ParticipationId} on language : {Language} ",
+                participation.Id, participation.Language);
+            return Task.CompletedTask;
         }
 
         public void WriteParticipation(ParticipationAggregate participation)
         {
+            Logger.LogInformation(
+                "Tests writing started for participation : {ParticipationId} on language : {Language} ",
+                participation.Id, participation.Language);
             TemplateDirectoryPath = _directoryService.GetTemplateDirectoryByParticipation(participation);
             FileStream.WriteLine(participation.GetExecutableCode());
             _fileStream?.Close();
             _fileStream = null;
+            Logger.LogInformation(
+                "Tests writing ended for participation : {ParticipationId} on language : {Language} ",
+                participation.Id, participation.Language);
         }
     }
 }
